2be5af4a6b04a96f375d48e2a2aca890b7f73c0f
Finis666 <yourstupid200@gmail.com>
2022-07-25 00:12:58 +0300

    s

--- api/middleware/adminAuth.js
+++ api/middleware/adminAuth.js
@@ -1,3 +1,32 @@
-function adminAuth(token) {}
+const jwt = require("jsonwebtoken");
+require("dotenv").config();
+const User = require("../models/User");
+async function adminAuth(req, res, next) {
+  const token = req.header("x-auth-token");
+  if (!token) {
+    res.send({ msg: "Access denied. No Token provided", success: false });
+    return;
+  }
+  try {
+    //checking if token valid
+    const decoded = jwt.verify(token, process.env.TOKENSECRET);
+    //checking if user is valid before continue
+    const checkUser = await User.findById(decoded.userId);
+    if (!checkUser) {
+      res.send({ msg: "User is not valid.", success: false });
+      return;
+    }
+    if(!checkUser.isAdmin) {
+        res.send({msg: "Access denied. You are not an administrator", success: false})
+        return;
+    }
+    
+    let decodedReq = { ...decoded, username: checkUser.username };
+    req.user = decodedReq;
+    next();
+  } catch (err) {
+    res.send({ msg: "Invalid token.", success: false });
+  }
+}
 
 module.exports = adminAuth;
--- api/routes/admin.js
+++ api/routes/admin.js
@@ -1,7 +1,6 @@
 const Router = require("express").Router();
 const adminAuth = require("../middleware/adminAuth.js");
-const auth = require("../middleware/auth");
 
-Router.get("/", [auth, adminAuth], (req, res) => {});
+Router.get("/", [adminAuth], (req, res) => {});
 
 module.exports = Router;
