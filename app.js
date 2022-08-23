507da2f42fa0fae2602b6cbb19045b6379cbb4ec
Finis666 <81584124+Finis666@users.noreply.github.com>
2022-08-23 23:09:50 +0300

    Cors dynemic url added

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ const server = http.createServer(app);
 const { Server } = require("socket.io");
 const io = new Server(server, {
   cors: {
-    origin: "http://localhost:3001",
+    origin: process.env.CLIENT_SIDE_URL,
     methods: ["GET", "POST"],
   },
 });
